8929af32668c32639a5fe8c7f0c534c8c1ec4219
Luca Greco <lgreco@mozilla.com>
2017-09-26 14:32:06 +0200

    fix: Add a new fallbackToNoCallback metadata property to customize the pageAction.show/hide wrappers behavior

--- src/browser-polyfill.js
+++ src/browser-polyfill.js
@@ -132,7 +132,39 @@ if (typeof browser === "undefined") {
         }
 
         return new Promise((resolve, reject) => {
-          target[name](...args, makeCallback({resolve, reject}, metadata));
+          function callAPIWithNoCallback() {
+            try {
+              target[name](...args);
+
+              // Update the API method metadata, so that the next API calls will not try to
+              // use the unsupported callback anymore.
+              metadata.fallbackToNoCallback = false;
+              metadata.noCallback = true;
+
+              resolve();
+            } catch (error) {
+              // Catch API parameters validation errors and rejects the promise.
+              reject(error);
+            }
+          }
+
+          if (metadata.fallbackToNoCallback) {
+            // This API method has currently no callback on Chrome, but it return a promise on Firefox,
+            // and so the polyfill will try to call it with a callback first, and it will fallback
+            // to not passing the callback if the first call fails.
+            try {
+              target[name](...args, makeCallback({resolve, reject}, metadata));
+            } catch (cbError) {
+              console.warn(`${name} API method doesn't seem to support the callback parameter, ` +
+                           "falling back to call it without a callback: ", cbError);
+
+              callAPIWithNoCallback();
+            }
+          } else if (metadata.noCallback) {
+            callAPIWithNoCallback();
+          } else {
+            target[name](...args, makeCallback({resolve, reject}, metadata));
+          }
         });
       };
     };
