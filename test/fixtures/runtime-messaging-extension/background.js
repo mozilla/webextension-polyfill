80a2fd5d3ccf257eae920065ba6a21c23c1e68b3
Luca Greco <lgreco@mozilla.com>
2018-03-13 14:16:09 +0100

    test: Added smoke test for pageAction.show fallback to no callback on chrome.

--- test/fixtures/runtime-messaging-extension/background.js
+++ test/fixtures/runtime-messaging-extension/background.js
@@ -5,6 +5,12 @@ console.log(name, "background page loaded");
 browser.runtime.onMessage.addListener((msg, sender) => {
   console.log(name, "background received msg", {msg, sender});
 
+  try {
+    browser.pageAction.show(sender.tab.id);
+  } catch (err) {
+    return Promise.resolve(`Unexpected error on pageAction.show: ${err}`);
+  }
+
   return Promise.resolve("background page reply");
 });
 
