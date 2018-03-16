9eace50e1bf27b050c50ec7d7ef5c7ca668f2f11
Luca Greco <lgreco@mozilla.com>
2018-03-16 20:20:11 +0100

    test: Added smoke test for the runtime.onMessage sendResponse behaviors

--- test/fixtures/runtime-messaging-extension/background.js
+++ test/fixtures/runtime-messaging-extension/background.js
@@ -2,16 +2,49 @@ const {name} = browser.runtime.getManifest();
 
 console.log(name, "background page loaded");
 
-browser.runtime.onMessage.addListener((msg, sender) => {
+browser.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   console.log(name, "background received msg", {msg, sender});
 
-  try {
-    browser.pageAction.show(sender.tab.id);
-  } catch (err) {
-    return Promise.resolve(`Unexpected error on pageAction.show: ${err}`);
-  }
+  switch (msg) {
+    case "test - sendMessage with returned Promise reply":
+      try {
+        browser.pageAction.show(sender.tab.id);
+      } catch (err) {
+        return Promise.resolve(`Unexpected error on pageAction.show: ${err}`);
+      }
 
-  return Promise.resolve("background page reply");
+      return Promise.resolve("bg page reply 1");
+
+    case "test - sendMessage with returned value reply":
+      // This is supposed to be ignored and the sender should receive
+      // a reply from the second listener.
+      return "Unexpected behavior: a plain return value should not be sent as a result";
+
+    case "test - sendMessage with synchronous sendResponse":
+      sendResponse("bg page reply 3");
+      return "value returned after calling sendResponse synchrously";
+
+    case "test - sendMessage with asynchronous sendResponse":
+      setTimeout(() => sendResponse("bg page reply 4"), 50);
+      return true;
+
+    case "test - second listener if the first does not reply":
+      // This is supposed to be ignored and the sender should receive
+      // a reply from the second listener.
+      return false;
+
+    default:
+      return Promise.resolve(
+        `Unxpected message received by the background page: ${JSON.stringify(msg)}\n`);
+  }
+});
+
+browser.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  setTimeout(() => {
+    sendResponse("second listener reply");
+  }, 100);
+
+  return true;
 });
 
 console.log(name, "background page ready to receive a content script message...");
