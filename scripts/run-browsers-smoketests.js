8d44ec7c51f608a70b4a9a208a6526cce1845473
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-01-16 23:45:00 +0100

    Address review comments

--- scripts/run-browsers-smoketests.js
+++ scripts/run-browsers-smoketests.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
+"use strict";
 const shell = require("shelljs");
 
+/**
+ * This is to make sure that even if the tests fail on Chrome,
+ * the tests still run on Firefox, so that it can be seen whether
+ * Firefox broke too or is unaffected.
+ */
 let result = 0;
 
 console.log(`
@@ -11,13 +17,11 @@ Test webextension-polyfill on real browsers
 // because Chrome doesn't currently support the extensions in headless mode)
 process.env.HEADLESS = 1;
 
-console.log(`
-Runing smoketests on Chrome`);
+console.log("\nRunning smoketests on Chrome");
 process.env.TEST_BROWSER_TYPE = "chrome";
 result = shell.exec("npm run test-integration:chrome").code || result;
 
-console.log(`
-Running smoketests on Firefox`);
+console.log("\nRunning smoketests on Firefox");
 process.env.TEST_BROWSER_TYPE = "firefox";
 result = shell.exec("npm run test-integration:firefox").code || result;
 
--- scripts/run-module-bundlers-smoketests.js
+++ scripts/run-module-bundlers-smoketests.js
@@ -7,16 +7,16 @@ console.log(`
 Test webextension-polyfill bundled with webpack
 ===============================================`);
 
-shell.exec("webpack --mode production --entry ./test/fixtures/bundle-entrypoint.js --output /tmp/webpack-bundle.js");
 process.env.TEST_BUNDLED_POLYFILL = "/tmp/webpack-bundle.js";
-result = shell.exec("npm run test").code || result;
+result = shell.exec(`webpack --mode production --entry ./test/fixtures/bundle-entrypoint.js --output ${process.env.TEST_BUNDLED_POLYFILL}`).code ||
+  shell.exec("npm run test").code || result;
 
 console.log(`
 Test webextension-polyfill bundled with browserify
 ==================================================`);
 
-shell.exec("browserify test/fixtures/bundle-entrypoint.js > /tmp/browserify-bundle.js");
 process.env.TEST_BUNDLED_POLYFILL = "/tmp/browserify-bundle.js";
-result = shell.exec("npm run test").code || result;
+result = shell.exec(`browserify test/fixtures/bundle-entrypoint.js > ${process.env.TEST_BUNDLED_POLYFILL}`).code ||
+  shell.exec("npm run test").code || result;
 
 process.exit(result);
