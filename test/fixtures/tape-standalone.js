82e287c0af29ad86821c56e219986b51a0f01127
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-02-28 21:00:00 +0100

    test: Fix test function timeout

--- test/fixtures/tape-standalone.js
+++ test/fixtures/tape-standalone.js
@@ -9,14 +9,50 @@ if (navigator.userAgent.includes("Chrome/")) {
   browser = "Firefox";
 }
 
-// Export as a global a wrapped test function which enforces a timeout by default.
-window.test = (desc, options, fn) => {
-  if (!fn && typeof options === "function") {
-    fn = options;
-    options = undefined;
+function getArgs(/* desc, options, fn */) {
+  let desc = "(anonymous)";
+  // By setting `timeout` to `0` here, we are allowing V8 to consider
+  // it a to have a signature of `{timeout: number}` instead of `{}`
+  // with a `timeout` property, which is easier on the C++ portion.
+  let options = {timeout: 0};
+  /** @type {function(any):void|Promise<void>} */
+  let fn;
+
+  for (let i = 0, arg; i < arguments.length; i++) {
+    arg = arguments[i];
+    switch (typeof arg) {
+      case "string":
+        desc = arg;
+        break;
+      case "function":
+        fn = arg;
+        break;
+      case "object":
+        Object.assign(options, arg);
+        break;
+    }
   }
+
+  if (!options.timeout || options.timeout < DEFAULT_TIMEOUT) {
+    options.timeout = DEFAULT_TIMEOUT;
+  }
+
+  return {
+    desc,
+    options,
+    fn,
+  };
+}
+
+// Export as a global a wrapped test function which enforces a timeout by default.
+/**
+ * @param {string} [desc]
+ * @param {object} [options]
+ * @param {function(any):void|Promise<void>} fn
+ */
+window.test = (desc, options, fn) => {
+  ({desc, options, fn} = getArgs(desc, options, fn));
   tape(`${desc} (${browser})`, options, async (t) => {
-    t.timeoutAfter(DEFAULT_TIMEOUT);
     await fn(t);
   });
 };
