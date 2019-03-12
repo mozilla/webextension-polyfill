58d85c762c77d07e9cc7bb0e3ac1698e5233837a
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-03-12 02:20:00 +0100

    test: Only allow swapping `fn` and `options` parameters

--- test/fixtures/tape-standalone.js
+++ test/fixtures/tape-standalone.js
@@ -9,50 +9,31 @@ if (navigator.userAgent.includes("Chrome/")) {
   browser = "Firefox";
 }
 
-function getArgs(/* desc, options, fn */) {
-  let desc = "(anonymous)";
-  // By setting `timeout` to `0` here, we are allowing V8 to consider
-  // it a to have a signature of `{timeout: number}` instead of `{}`
-  // with a `timeout` property, which is easier on the C++ portion.
-  let options = {timeout: 0};
-  /** @type {function(import("tape").Test):void|Promise<void>} */
-  let fn;
-
-  for (let i = 0, arg; i < arguments.length; i++) {
-    arg = arguments[i];
-    switch (typeof arg) {
-      case "string":
-        desc = arg;
-        break;
-      case "function":
-        fn = arg;
-        break;
-      case "object":
-        Object.assign(options, arg);
-        break;
-    }
-  }
-
-  if (!options.timeout) {
-    options.timeout = DEFAULT_TIMEOUT;
-  }
-
-  return {
-    desc,
-    options,
-    fn,
-  };
-}
-
 // Export as a global a wrapped test function which enforces a timeout by default.
 // eslint-disable-next-line
 /**
- * @param {string} [desc]
- * @param {object} [options]
+ * @param {string} desc
+ * @param {import("tape").TestOptions|null} [options]
  * @param {(test: import("tape").Test) => void|Promise<void>} fn
  */
 window.test = (desc, options, fn) => {
-  ({desc, options, fn} = getArgs(desc, options, fn));
+  if (typeof options === "function") {
+    // Allow swapping options with fn
+    if (typeof fn === "object" && fn) {
+      let tmp = fn;
+      fn = options;
+      options = tmp;
+    } else {
+      fn = options;
+      options = undefined;
+    }
+  }
+
+  options = {
+    timeout: DEFAULT_TIMEOUT,
+    ...options,
+  };
+
   tape(`${desc} (${browser})`, options, async (t) => {
     await fn(t);
   });
