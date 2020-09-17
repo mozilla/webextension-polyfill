d6a789bf544636936009c0817bcd9467121ffe7e
Rob Wu <rob@robwu.nl>
2020-09-17 12:58:54 +0200

    fix(test): fix expectation of tabs-sendmessage-expectation in Firefox
    Work-around for upstream regression @ https://bugzilla.mozilla.org/show_bug.cgi?id=1665568

--- test/fixtures/tabs-sendmessage-extension/content.js
+++ test/fixtures/tabs-sendmessage-extension/content.js
@@ -1,8 +1,19 @@
 test("tabs.sendMessage reject when sending to unknown tab id", async (t) => {
   const res = await browser.runtime.sendMessage("test-tabssendMessage-unknown-tabid");
+  let errorMessage = "Could not establish connection. Receiving end does not exist.";
+  const firefoxVersion = +(/Firefox\/([0-9]+)/.exec(navigator.userAgent) || ["", "0"])[1];
+  if (firefoxVersion === 79) {
+    // Value of error message regressed in:
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=1665568
+    errorMessage = "Error: tab is null";
+  } else if (firefoxVersion >= 80) {
+    // Raw error message leaked until it got sanitized again with
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=1655624
+    errorMessage = "An unexpected error occurred";
+  }
   t.deepEqual(res, {
     success: true,
     isRejected: true,
-    errorMessage: "Could not establish connection. Receiving end does not exist.",
+    errorMessage,
   }, "The background page got a rejection as expected");
 });
