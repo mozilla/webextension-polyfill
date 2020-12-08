3ba72c96a98a2ad79309750b5b730154484ce882
Rob Wu <rob@robwu.nl>
2020-12-08 15:34:51 +0100

    fix(test): fix expectation of tabs-sendmessage in Firefox (#251)
    Upstream regression got fixed @ https://bugzilla.mozilla.org/show_bug.cgi?id=1665568

--- test/fixtures/tabs-sendmessage-extension/content.js
+++ test/fixtures/tabs-sendmessage-extension/content.js
@@ -4,9 +4,11 @@ test("tabs.sendMessage reject when sending to unknown tab id", async (t) => {
   const firefoxVersion = +(/Firefox\/([0-9]+)/.exec(navigator.userAgent) || ["", "0"])[1];
   if (firefoxVersion === 79) {
     // Value of error message regressed in:
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=1583484
+    // and got fixed in Firefox 83 in:
     // https://bugzilla.mozilla.org/show_bug.cgi?id=1665568
     errorMessage = "Error: tab is null";
-  } else if (firefoxVersion >= 80) {
+  } else if (firefoxVersion >= 80 && firefoxVersion < 83) {
     // Raw error message leaked until it got sanitized again with
     // https://bugzilla.mozilla.org/show_bug.cgi?id=1655624
     errorMessage = "An unexpected error occurred";
