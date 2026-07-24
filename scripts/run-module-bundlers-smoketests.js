aad9d3f4722b8659b9689ff58b353bb0256d99a3
orbisai0security <mediratta@gmail.com>
2026-07-24 10:28:49 +0000

    fix: javascript.shelljs.security.shelljs-exec-injection.shelljs-exec-injection security vulnerability
    Automated security fix generated by OrbisAI Security

--- scripts/run-module-bundlers-smoketests.js
+++ scripts/run-module-bundlers-smoketests.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-const path = require("path");
 const shell = require("shelljs");
 
 let result = 0;
@@ -9,10 +8,8 @@ Test webextension-polyfill bundled with webpack
 ===============================================`);
 
 process.env.TEST_BUNDLED_POLYFILL = "/tmp/webpack-bundle.js";
-const webpackOutputDir = path.dirname(process.env.TEST_BUNDLED_POLYFILL);
-const webpackOutputFilename = path.basename(process.env.TEST_BUNDLED_POLYFILL);
 
-result = shell.exec(`webpack --mode production --entry ./test/fixtures/bundle-entrypoint.js --output-path ${webpackOutputDir} --output-filename ${webpackOutputFilename}`).code ||
+result = shell.exec("webpack --mode production --entry ./test/fixtures/bundle-entrypoint.js --output-path /tmp --output-filename webpack-bundle.js").code ||
   shell.exec("npm run test").code || result;
 
 console.log(`
@@ -20,7 +17,7 @@ Test webextension-polyfill bundled with browserify
 ==================================================`);
 
 process.env.TEST_BUNDLED_POLYFILL = "/tmp/browserify-bundle.js";
-result = shell.exec(`browserify test/fixtures/bundle-entrypoint.js > ${process.env.TEST_BUNDLED_POLYFILL}`).code ||
+result = shell.exec("browserify test/fixtures/bundle-entrypoint.js > /tmp/browserify-bundle.js").code ||
   shell.exec("npm run test").code || result;
 
 process.exit(result);
